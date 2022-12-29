f75fbdb95a90bb01af5d3df53e150da3582690be
Vitor Hugo Pinto <vhpinto1@gmail.com>
2022-12-29 15:05:23 -0300

    Fundamentos do ReactJS com TS

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import { Header } from './components/Header';
+import { Post } from './components/Post';
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css'
+
+import './global.css';
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/vhpinto.png',
+      name: 'Vítor Hugo',
+      role: 'Web Developer',
+    },
+    content: [
+      { type: 'paragraph', line: 'Fala galeraa 👋'},
+      { type: 'paragraph', line: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', line: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-12-18 21:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/arkaisho.png',
+      name: 'João Pedro',
+      role: 'Mobile Developer @Loomi',
+    },
+    content: [
+      { type: 'paragraph', line: 'Fala galeraa 👋'},
+      { type: 'paragraph', line: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', line: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-12-20 13:00:00'),
+  },
+];
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map(post => {
+            return (
+              <Post 
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
+        </main>
+      </div>
+    </div>
+  )
+}
