071df2d90bc4be8f0e1af267b287466183e7cd6a
Vitor Hugo Pinto <vhpinto1@gmail.com>
2023-01-17 17:51:47 -0300

    Mudança de formatação do arquivo

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,27 +1,27 @@
-import { PencilLine } from 'phosphor-react';
-import { Avatar } from './Avatar';
+import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
 
-import styles from './Sidebar.module.css';
+import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
-    return (
-        <aside className={styles.sidebar}>
-            <img 
-                className={styles.cover}
-                src='https://images.unsplash.com/photo-1661956602868-6ae368943878?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=40'
-            />
-            <div className={styles.profile}>
-                <Avatar src='https://github.com/vhpinto.png' />
-                <strong>Vitor Hugo</strong>
-                <span>Web Developer</span>
-            </div>
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1661956602868-6ae368943878?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=40"
+      />
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/vitorhugo-pinto.png" />
+        <strong>Vitor Hugo</strong>
+        <span>Web Developer</span>
+      </div>
 
-            <footer>
-                <a href='#'>
-                    <PencilLine size={20}/>
-                    Editar seu perfil
-                </a>
-            </footer>
-        </aside>
-    );
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
 }
