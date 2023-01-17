c175b54141b2f8205fbbd20f4d4ad469fed31544
Vitor Hugo Pinto <vhpinto1@gmail.com>
2023-01-17 17:52:06 -0300

    Mudança no caminho do src

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,54 +1,54 @@
-import styles from './Comment.module.css';
-import { Trash, HandsClapping } from 'phosphor-react';
-import { Avatar } from './Avatar';
-import { useState } from 'react';
+import { HandsClapping, Trash } from "phosphor-react";
+import { useState } from "react";
+import { Avatar } from "./Avatar";
+import styles from "./Comment.module.css";
 
 interface CommentProps {
-    content: string;
-    onDeleteComment: (content: string) => void;
+  content: string;
+  onDeleteComment: (content: string) => void;
 }
 
 export function Comment({ content, onDeleteComment }: CommentProps) {
-    const [ claps, setClaps ] = useState(0);
+  const [claps, setClaps] = useState(0);
 
-    function handleDeleteComment() {
-        onDeleteComment(content);
-    }
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
 
-    function handleClapsCount() {
-        setClaps((prev)=> {
-            return prev + 1;
-        });
-    }
+  function handleClapsCount() {
+    setClaps((prev) => {
+      return prev + 1;
+    });
+  }
 
-    return(
-        <div className={styles.comment}>
-            <Avatar hasBorder={false} src='https://github.com/vhpinto.png' />
-            
-            <div className={styles.commentBox}>
-                <div className={styles.commentTextArea}>
-                    <header>
-                        <div className={styles.commentAuthorAndTime}>
-                            <strong>Vitor Hugo</strong>
-                            <time title='11 de maio às 08:13' dateTime='2022-05-11 08:13:30'>Cerca de 1h atrás</time>
-                        </div>
-                        <button onClick={handleDeleteComment}  title='Deletar comentário'>
-                            <Trash size={24} />
-                        </button>
-                    </header>
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/vitorhugo-pinto.png" />
 
-                    <p>{content}</p>
-                </div>
-                <footer>
-                    <button 
-                        onClick={handleClapsCount}
-                        title='Aplaudir comentário'>
-                        <HandsClapping size={24} />
-                        Aplaudir
-                        <span>{claps}</span>
-                    </button>
-                </footer>
+      <div className={styles.commentBox}>
+        <div className={styles.commentTextArea}>
+          <header>
+            <div className={styles.commentAuthorAndTime}>
+              <strong>Vitor Hugo</strong>
+              <time title="11 de maio às 08:13" dateTime="2022-05-11 08:13:30">
+                Cerca de 1h atrás
+              </time>
             </div>
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>{content}</p>
         </div>
-    )
+        <footer>
+          <button onClick={handleClapsCount} title="Aplaudir comentário">
+            <HandsClapping size={24} />
+            Aplaudir
+            <span>{claps}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
 }
