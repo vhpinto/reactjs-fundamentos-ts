960a57ef89113447859d5df3919408ab4e4597c5
Vitor Hugo Pinto <vhpinto1@gmail.com>
2023-01-17 17:52:32 -0300

    Mudança de formatação do arquivo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
-import styles from './Header.module.css';
-import igniteLogo from '../assets/ignite-logo.svg'
+import igniteLogo from "../assets/ignite-logo.svg";
+import styles from "./Header.module.css";
 
 export function Header() {
-    return (
-        <header  className={styles.header}>
-            <img src={igniteLogo} alt="Logotipo do ignite"></img>
-        </header>
-    );
+  return (
+    <header className={styles.header}>
+      <img src={igniteLogo} alt="Logotipo do ignite"></img>
+    </header>
+  );
 }
